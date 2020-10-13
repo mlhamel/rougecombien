263af4263d47eab82bd8482ae7a29c62523341ed
Mathieu Leduc-Hamel <mathieu.leduchamel@shopify.com>
2020-10-12 21:31:10 -0400

    Simplify the command and initialize AppEngine configuration.

--- cmd/rougecombien/main.go
+++ cmd/rougecombien/main.go
@@ -4,31 +4,27 @@ import (
 	"context"
 	"os"
 
-	"github.com/mlhamel/rougecombien/pkg/config"
-
 	"github.com/pior/runnable"
 	"github.com/urfave/cli"
 
+	"github.com/mlhamel/rougecombien/pkg/config"
 	"github.com/mlhamel/rougecombien/pkg/web"
 )
 
 func main() {
-	cliApp := cli.NewApp()
 	cfg := config.NewConfig()
 	ctx := context.Background()
 
-	cliApp.Commands = []cli.Command{
-		{
-			Name: "run",
-			Action: func(*cli.Context) error {
-				manager := runnable.Manager(nil)
-				manager.Add(web.NewController(cfg))
-				return manager.Build().Run(ctx)
-			},
+	app := cli.App{
+		Name: "rougecombien",
+		Action: func(*cli.Context) error {
+			manager := runnable.Manager(nil)
+			manager.Add(web.NewController(cfg))
+			return manager.Build().Run(ctx)
 		},
 	}
 
-	if err := cliApp.Run(os.Args); err != nil {
+	if err := app.Run(os.Args); err != nil {
 		panic(err)
 	}
 }
